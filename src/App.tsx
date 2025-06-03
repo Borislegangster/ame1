c04eab2e69f8956aa557264aca3140b1d51cc04b
Borislegangster <144940956+Borislegangster@users.noreply.github.com>
2025-06-03 03:15:38 +0100

    Fix App component export and routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-```typescript
-// Ajouter la route pour la gestion des utilisateurs
-<Route
-  path="/admin/users"
-  element={
-    <ProtectedRoute allowedRoles={['admin']}>
-      <UserList />
-    </ProtectedRoute>
-  }
-/>
-```
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserList } from './pages/admin/users/UserList';
+import { ProtectedRoute } from './components/admin/ProtectedRoute';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Admin routes */}
+        <Route
+          path="/admin/users"
+          element={
+            <ProtectedRoute allowedRoles={['admin']}>
+              <UserList />
+            </ProtectedRoute>
+          }
+        />
+        
+        {/* Add other routes here */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
